0850d24af35ce42a3d807e5bbac0425e2431c5ea
seo <seotownsend@icloud.com>
2016-06-14 11:37:47 -0700

    Added request logic to apple's servers

--- apple.go
+++ apple.go
@@ -1,8 +1,13 @@
 package yiap
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -49,6 +54,78 @@ func NewAppleReceiptResponseFromData(resp []byte) (*AppleReceiptResponse, error)
 	}
 
 	return &receipt, nil
+}
+
+var specOverrideAppleIAPRequestEndpoint string
+
+// Contacts apples servers and retrieves a receipt. Password is optional
+// for non-subscription type receipts
+func ProcessAppleIAPRequestPayload(payload []byte, password string, isProduction bool) (*AppleReceiptResponse, error) {
+	payloadStr := strings.TrimSpace(string(payload)) // Payload must not have a newline
+	info := map[string]interface{}{
+		"receipt-data": payloadStr,
+		"password":     password,
+	}
+
+	infoJson, err := json.Marshal(info)
+	if err != nil {
+		return nil, fmt.Errorf("ProcessAppleIAPRequestPayload: Tried to marhshal payload '%s'...: %s", payloadStr[0:10], err)
+	}
+
+	var url string
+
+	if specOverrideAppleIAPRequestEndpoint == "" {
+		if isProduction {
+			url = "https://buy.itunes.apple.com/verifyReceipt"
+		} else {
+			url = "https://sandbox.itunes.apple.com/verifyReceipt"
+		}
+	} else {
+		url = specOverrideAppleIAPRequestEndpoint
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(infoJson))
+	if err != nil {
+		return nil, fmt.Errorf("ProcessAppleIAPRequestPayload: Tried to create a request but this failed: %s", err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("ProcessAppleIAPRequestPayload: Tried to execute request but this failed: %s", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("ProcessAppleIAPRequestPayload: Tried to read request buffer but this failed: %s", err)
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("ProcessAppleIAPRequestPayload: Tried to read request from apple but got a non-200 status code (got '%d'). Request payload was: '%s', Apple's response payload was: '%s'", payloadStr[:10], resp.StatusCode, body)
+	}
+
+	receipt, err := NewAppleReceiptResponseFromData(body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Determine if the returned status code is invalid. We don't consider the 21006 case (expired subscription)
+	// as the receipt is still processed correctly under those circumstances and it doesn't really make sense.
+	iapReceiptCodes := map[int]string{
+		21000: "The App Store could not read the JSON object you provided.",
+		21002: "The data in the receipt-data property was malformed or missing.",
+		21003: "The receipt could not be authenticated.",
+		21004: "The shared secret you provided does not match the shared secret on file for your account.",
+		21005: "The receipt server is not currently available.",
+		21007: "This receipt is from the test environment, but it was sent to the production environment for verification. Send it to the test environment instead.",
+		21008: "This receipt is from the production environment, but it was sent to the test environment for verification. Send it to the production environment instead.",
+	}
+	if codeMsg, ok := iapReceiptCodes[receipt.Status]; ok {
+		return nil, fmt.Errorf("ProcessAppleIAPRequestPayload failed to process receipt, it did get a 200 response back from apple but the status code on the receipt was invalid (%d) with request payload: '%s', response payload: '%s'.  Apple's reason for error is: '%s", payloadStr[:10], receipt.Status, body[:100], codeMsg)
+	}
+
+	return receipt, nil
 }
 
 /*
--- apple_test.go
+++ apple_test.go
@@ -1,6 +1,10 @@
 package yiap
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -27,6 +31,17 @@ func TestYiap(t *testing.T) {
 		So(txns[0].GetQuantity(), ShouldEqual, 1)
 	})
 
+	Convey("Can decode another apple receipt", t, func() {
+		responseCode := getFixtureWithPath("apple/receipt2_response.json")
+
+		receipt, err := NewAppleReceiptResponseFromData([]byte(responseCode))
+		checkErr(err)
+
+		So(receipt.GetStatus(), ShouldEqual, false)
+		So(receipt.EnvironmentIsSandbox(), ShouldEqual, true)
+		So(len(receipt.GetTransactions()), ShouldEqual, 1)
+	})
+
 	// Sometimes? apple receipts may contain entries in in-app seciton of the receipt which do not
 	// co-incide with the subscription results in `latest_receipt_info`.  in this cases, we are
 	// going to find all unique transaction items (identified by the transaction_id and then take
@@ -44,5 +59,71 @@ func TestYiap(t *testing.T) {
 
 		// 7 Unique transactions (6 in the `latest_receipt_info` and 1 in the `in_app` purchase section.
 		So(len(receipt.GetTransactions()), ShouldEqual, 7)
+	})
+
+	Convey("Can request a receipt to be verified & decoded from apple with the latest version", t, func() {
+		requestPayload := getFixtureWithPath("apple/receipt2_request")
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			fmt.Fprintln(w, getFixtureWithPath("apple/receipt2_response.json"))
+		}))
+		defer ts.Close()
+		url := ts.URL
+
+		// Sends the payload off to apple where it is processed and returned as a
+		// JSON receipt
+		password := "password"
+		specOverrideAppleIAPRequestEndpoint = url
+		receipt, err := ProcessAppleIAPRequestPayload([]byte(requestPayload), password, false)
+		checkErr(err)
+
+		So(receipt.GetStatus(), ShouldEqual, false)
+		So(receipt.EnvironmentIsSandbox(), ShouldEqual, true)
+
+		// 1 Unique Transaction as we're returning the response directly
+		So(len(receipt.GetTransactions()), ShouldEqual, 1)
+		specOverrideAppleIAPRequestEndpoint = ""
+	})
+
+	Convey("If apple replies with a non-200x error code, then the error is returned", t, func() {
+		requestPayload := getFixtureWithPath("apple/receipt2_request")
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+
+			// Return a bad request
+			http.Error(w, "uh oh", http.StatusBadRequest)
+		}))
+		defer ts.Close()
+		url := ts.URL
+
+		// Send our bogus request
+		password := "password"
+		specOverrideAppleIAPRequestEndpoint = url
+		_, err := ProcessAppleIAPRequestPayload([]byte(requestPayload), password, false)
+		So(err, ShouldNotEqual, nil)
+		So(strings.Contains(fmt.Sprintf("%s", err), "uh oh"), ShouldEqual, true)
+
+		specOverrideAppleIAPRequestEndpoint = ""
+	})
+
+	Convey("Does throw an error when contacting apple about an invalid subscription receipt", t, func() {
+		requestPayload := getFixtureWithPath("apple/receipt2_request")
+
+		// Forward a bogus password so our request fails
+		password := "password"
+		_, err := ProcessAppleIAPRequestPayload([]byte(requestPayload), password, true)
+
+		// Should fail with a 21007 error (should not be in a production environment)
+		So(err, ShouldNotEqual, nil)
+		So(strings.Contains(fmt.Sprintf("%s", err), "21007"), ShouldEqual, true)
+
+		_, err = ProcessAppleIAPRequestPayload([]byte(requestPayload), password, false)
+
+		// Password is invalid
+		So(err, ShouldNotEqual, nil)
+		So(strings.Contains(fmt.Sprintf("%s", err), "21004"), ShouldEqual, true)
+
 	})
 }
